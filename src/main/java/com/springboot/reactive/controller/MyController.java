ddeb04f7d5be76af2dfe0004a325c1072c43af8c
Reggie <reggie.senal@gmail.com>
2024-08-20 19:39:36 +0800

    simple get request calling 2 mono calls consecutively

---
+++ src/main/java/com/springboot/reactive/controller/MyController.java
@@ -0,0 +1,31 @@
+package com.springboot.reactive.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@RestController
+public class MyController {
+
+    // call 2 mono function immediately executed one after another even though the browser waits to be completed
+    @GetMapping("/demo")
+    public Mono<String> getDemo() {
+        return simpleMonoHello()
+                .zipWith(anotherMonoHello())
+                .map(value -> value.getT1() + " " + value.getT2());
+    }
+
+    private Mono<String> simpleMonoHello() {
+        System.out.println("simple here");
+        return Mono.just("Hello There!")
+                .delayElement(Duration.ofSeconds(5));
+    }
+
+    private Mono<String> anotherMonoHello() {
+        System.out.println("another message here");
+        return Mono.just("Greeting!!")
+                .delayElement(Duration.ofSeconds(5));
+    }
+}
