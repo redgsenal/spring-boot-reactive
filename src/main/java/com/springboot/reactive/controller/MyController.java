5f227833ac2b5c4dcf4325f51956dda8d267ad8c
Reggie <reggie.senal@gmail.com>
2024-08-20 20:20:02 +0800

    add templates and demo pages

--- src/main/java/com/springboot/reactive/controller/MyController.java
+++ src/main/java/com/springboot/reactive/controller/MyController.java
@@ -1,31 +1,17 @@
 package com.springboot.reactive.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
-import java.time.Duration;
+import java.time.LocalDateTime;
 
-@RestController
+@Controller
 public class MyController {
 
-    // call 2 mono function immediately executed one after another even though the browser waits to be completed
-    @GetMapping("/demo")
-    public Mono<String> getDemo() {
-        return simpleMonoHello()
-                .zipWith(anotherMonoHello())
-                .map(value -> value.getT1() + " " + value.getT2());
-    }
-
-    private Mono<String> simpleMonoHello() {
-        System.out.println("simple here");
-        return Mono.just("Hello There!")
-                .delayElement(Duration.ofSeconds(5));
-    }
-
-    private Mono<String> anotherMonoHello() {
-        System.out.println("another message here");
-        return Mono.just("Greeting!!")
-                .delayElement(Duration.ofSeconds(5));
+    @GetMapping("/hello")
+    public String sayHello(Model model) {
+        model.addAttribute("theDate", LocalDateTime.now());
+        return "helloworld";
     }
 }
