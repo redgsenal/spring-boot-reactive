ba1e74e3b97a6095c7a210d955c085a3fbf27f19
Reggie <reggie.senal@gmail.com>
2024-08-20 19:08:46 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spring Boot Reactive
+
+34 - Creating a reactive Spring Boot app (Reactive programming with Java - full course)
+https://www.youtube.com/watch?v=II52GMXir4E&list=PLqq-6Pq4lTTYPR2oH7kgElMYZhJd4vOGI&index=30
---
+++ src/main/java/com/springboot/reactive/ReactiveSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringbootApplication.class, args);
+	}
+
+}
